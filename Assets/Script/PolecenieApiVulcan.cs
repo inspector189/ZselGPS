a7773115677bdd721f80f2618c6767760ea3dd65
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2023-11-28 19:54:52 +0100

    komit

--- Assets/Script/PolecenieApiVulcan.cs
+++ Assets/Script/PolecenieApiVulcan.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine; // Importuj przestrzeñ nazw UnityEngine
@@ -12,13 +13,19 @@ using Vulcanova.Uonet.Crypto;
 using Vulcanova.Uonet.Firebase;
 using Vulcanova.Uonet.Signing;
 
+public class User
+{
+    public string Token { get; set; }
+    public string Pin { get; set; }
+    // Dodaj inne dane użytkownika, np. imię, nazwisko, itp.
+}
 public class PolecenieApiVulcan : MonoBehaviour
 {
     public TextMeshProUGUI textMesh; // Przypisz pole TextMeshProUGUI z Unity Inspector
     public TMP_InputField tokenInputField;
     public string symbol = "wloclawek";
     public TMP_InputField pinInputField;
-
+    private List<User> usersList = new List<User>();
     public async void LoginProcess()
     {
         string token = tokenInputField.text; // Pobierz wartość tokenu z InputFielda
@@ -35,22 +42,36 @@ public class PolecenieApiVulcan : MonoBehaviour
         var instanceUrlProvider = new InstanceUrlProvider();
 
         var apiClient = new ApiClient(requestSigner, await instanceUrlProvider.GetInstanceUrlAsync(token, symbol));
-
-        var request = new RegisterClientRequest
+        try
         {
-            OS = Constants.AppOs,
-            DeviceModel = Constants.DeviceModel,
-            Certificate = Convert.ToBase64String(x509Cert2.RawData),
-            CertificateType = "X509",
-            CertificateThumbprint = x509Cert2.Thumbprint,
-            PIN = pin,
-            SecurityToken = token,
-            SelfIdentifier = Guid.NewGuid().ToString()
+            var request = new RegisterClientRequest
+            {
+                OS = Constants.AppOs,
+                DeviceModel = Constants.DeviceModel,
+                Certificate = Convert.ToBase64String(x509Cert2.RawData),
+                CertificateType = "X509",
+                CertificateThumbprint = x509Cert2.Thumbprint,
+                PIN = pin,
+                SecurityToken = token,
+                SelfIdentifier = Guid.NewGuid().ToString()
+            };
+
+            await apiClient.PostAsync(RegisterClientRequest.ApiEndpoint, request);
+            User newUser = new User
+            {
+                Token = token,
+                Pin = pin,
+                // Dodaj inne dane użytkownika pobrane z dziennika
+               
         };
+            usersList.Add(newUser);
+        }
+      catch (Exception e)
+        {
+            Debug.Log("Tworzenie użytkownika nie powiodło się: " + e.Message);
+        }
 
-        await apiClient.PostAsync(RegisterClientRequest.ApiEndpoint, request);
-
-        var registerHebeResponse = await apiClient.GetAsync(RegisterHebeClientQuery.ApiEndpoint, new RegisterHebeClientQuery());
+var registerHebeResponse = await apiClient.GetAsync(RegisterHebeClientQuery.ApiEndpoint, new RegisterHebeClientQuery());
 
         var firstAccount = registerHebeResponse.Envelope[0];
 
