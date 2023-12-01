f5d240f0acb58e4b988e9802328a5afdbe1f9fda
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2023-12-01 15:50:34 +0100

    commit

--- Assets/Script/AveragePosition.cs
+++ Assets/Script/AveragePosition.cs
@@ -1,24 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using TMPro;
 using UnityEngine;
 
 public class AveragePosition
 {
-    //Klasa wylicza œredni¹ pozycjê GPS z ostatnich maxAver pomiarów
     const int maxAver = 10;
-
-    //standardowo pomiar jest dodawany na koniec listy i gdy liczba elementów przekracza maxAver usuwany jest pierwszy element z kolejki
-    //sumPosition jest powiêkszany o dodawany element i gdy z kolejki jest usuwany element to jest pomniejszany o usuwany element
-    //Przyspiesza to dzia³anie programu lecz pewnym czasie powstaj¹ b³êdy zaokr¹gleñ. Dlatego po maxCount operacji ca³a kolejka jest przeliczana
     const int maxCount = 1000;
-    public AveragePosition avgPosition;
     private int lastRecalculateFrame = 0;
     private float totalWeight = 0.0f;
     const int maxRecalculateFrameCount = 1000;
     const int maxAverageMeasurements = 10;
-
+    private int count = 0; // Zmienna count powinna byæ zadeklarowana raz
+    private Queue<Vector2> positions = new Queue<Vector2>(); // Kolejka positions powinna byæ zadeklarowana raz
+    private Vector2 sumPosition = new Vector2(0.0f, 0.0f); // Zmienna sumPosition powinna byæ zadeklarowana raz
 
     public void AddWeightedPosition(Vector2 pos, float weight)
     {
@@ -33,15 +26,19 @@ public class AveragePosition
 
         return Vector2.zero;
     }
+
     private void RecalculateQueue()
     {
         sumPosition = Vector2.zero;
+        totalWeight = 0.0f; // Dodano resetowanie totalWeight podczas przeliczania kolejki
         foreach (Vector2 v in positions)
         {
             sumPosition += v;
+            totalWeight += 1; // Dodano zwiêkszanie totalWeight dla ka¿dej pozycji
         }
-        count = 0;
+        count = 0; // Resetowanie count jest konieczne, aby unikn¹æ ponownego przeliczania
     }
+
     private void RecalculateQueueIfNeeded()
     {
         if (Time.frameCount - lastRecalculateFrame > maxRecalculateFrameCount)
@@ -50,45 +47,34 @@ public class AveragePosition
             lastRecalculateFrame = Time.frameCount;
         }
     }
+
     public void AddPosition(Vector2 pos)
     {
-        //dodajemy do sumy aktualn¹ pozycjê
         sumPosition += pos;
         positions.Enqueue(pos);
+        totalWeight += 1; // Zwiêkszanie totalWeight przy dodawaniu nowej pozycji
 
         if (positions.Count > maxAverageMeasurements)
         {
             Vector2 removedPos = positions.Dequeue();
             sumPosition -= removedPos;
+            totalWeight -= 1; // Zmniejszanie totalWeight przy usuwaniu pozycji
         }
+
         RecalculateQueueIfNeeded();
-        //positions.Clear();
-        //gdy licznik przekroczy max count przeliczamy ca³¹ kolejkê aby unikn¹æ b³êdów zaokr¹gleñ
+
         count++;
         if (count > maxCount)
         {
-            sumPosition = Vector2.zero;
-            foreach (Vector2 v in positions)
-            {
-                sumPosition += v;
-            }
-            count = 0;
+            RecalculateQueue();
         }
     }
 
-
-
     public Vector2 GetAveragePosition()
     {
-        if (positions.Count > 0) //pamiêtaj cholero nie dziel przez zero
+        if (positions.Count > 0)
             return sumPosition / positions.Count;
 
         return Vector2.zero;
     }
-
-    private int count = 0;
-    Queue<Vector2> positions = new Queue<Vector2>();
-
-    Vector2 sumPosition = new Vector2(0.0f, 0.0f);
-
 }
--- Assets/Script/GPSMap2.cs
+++ Assets/Script/GPSMap2.cs
@@ -4,6 +4,144 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Android;
 using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class GPSMap2 : MonoBehaviour
+{
+    // Unity object representing the person on the map.
+    public GameObject personObject;
+
+    // Map dimensions and scaling factors
+    private float mapWidth = 3557f; // Unity units
+    private float mapHeight = 2789.9f; // Unity units
+    private Vector2 mapOrigin = new Vector2(2, -60.8f); // Unity units
+    private float realMapWidth = 10527f; // meters, adjust if needed
+    private float realMapHeight = 7443f; // meters, adjust if needed
+
+    // Store the last 10 location readings
+    private Queue<Vector2> locationReadings = new Queue<Vector2>();
+    private int maxReadings = 10;
+
+    void Start()
+    {
+        // Start location service before querying location
+        StartCoroutine(StartLocationService());
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        // Check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("Location services are not enabled by the user");
+            yield break;
+        }
+
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        // Access granted and location value could be retrieved
+        Debug.Log("Location: " + Input.location.lastData.latitude + " " +
+                  Input.location.lastData.longitude);
+
+        // Start the update loop
+        StartCoroutine(UpdateLocation());
+    }
+
+    private IEnumerator UpdateLocation()
+    {
+        while (true)
+        {
+            UpdatePersonPosition(Input.location.lastData.latitude, Input.location.lastData.longitude);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    private void UpdatePersonPosition(float latitude, float longitude)
+    {
+        // Convert latitude and longitude to unity world space
+        Vector2 unityPosition = ConvertGPSToUnity(latitude, longitude);
+
+        // Add this reading to the queue
+        if (locationReadings.Count >= maxReadings)
+        {
+            locationReadings.Dequeue(); // Remove the oldest reading
+        }
+        locationReadings.Enqueue(unityPosition); // Add the new reading
+
+        // Calculate the average position
+        Vector2 averagePosition = Vector2.zero;
+        foreach (Vector2 pos in locationReadings)
+        {
+            averagePosition += pos;
+        }
+        averagePosition /= locationReadings.Count;
+
+        // Set the person object's position
+        personObject.transform.position = new Vector3(averagePosition.x, averagePosition.y, 0);
+    }
+
+    private Vector2 ConvertGPSToUnity(float latitude, float longitude)
+    {
+        // Replace these with the GPS coordinates of your map's origin
+        float originLatitude = 0.0f;
+        float originLongitude = 0.0f;
+
+        // Calculate the offset from the origin
+        float latOffset = (latitude - originLatitude) * (realMapHeight / Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * latitude) * 111320f));
+        float lonOffset = (longitude - originLongitude) * (realMapWidth / 111320f);
+
+        // Scale this offset according to the Unity map size
+        float unityLat = latOffset * (mapHeight / realMapHeight);
+        float unityLon = lonOffset * (mapWidth / realMapWidth);
+
+        // Adjust by the map's origin in Unity
+        return new Vector2(unityLon + mapOrigin.x, unityLat + mapOrigin.y);
+    }
+
+    void OnApplicationQuit()
+    {
+        // Stop the location service if it's running
+        if (Input.location.isEnabledByUser)
+        {
+            Input.location.Stop();
+        }
+    }
+}
+
+
+
+
+
+
+/*using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
 
 
 public class GPSMap2 : MonoBehaviour
@@ -152,43 +290,9 @@ public class GPSMap2 : MonoBehaviour
         Vector2 act = avg.GetAveragePosition();
         double rlong = 111200.0f;
         double ralt = Math.Cos(Math.PI * act.y / 180.0f) * Math.PI * r / 180.0f;
-
         Vector2 dorigin = new Vector2(((float)((act.x - origin_longi) * ralt)), ((float)((act.y - origin_lati) * rlong)));
-
-        Debug.Log(origin_longi + " " + origin_lati);
-
-        if(dorigin.x < 0 && dorigin.y < 0) // lewy dolny (pracownie itd)
-        {
-            Vector2 unityPosition = new Vector2(dorigin.x, dorigin.y);
-
-            return mapa.transform.TransformPoint(unityPosition);
-        }
-
-        if (dorigin.x > 0 && dorigin.y < 0) // prawy górny (basen itd)
-        {
-            Vector2 unityPosition = new Vector2(dorigin.x * 15.7f, (dorigin.y) + 650);
-
-            return mapa.transform.TransformPoint(unityPosition);
-        }
-        if (dorigin.x < 0 && dorigin.y > 0) // lewy górny (hala itd)
-        {
-            Vector2 unityPosition = new Vector2(dorigin.x, dorigin.y);
-
-            return mapa.transform.TransformPoint(unityPosition);
-        }
-        if (dorigin.x > 0 && dorigin.y > 0) 
-        {
-            Vector2 unityPosition = new Vector2(dorigin.x, dorigin.y);
-
-            return mapa.transform.TransformPoint(unityPosition);
-        }
-
-
-        Vector2 unityPosition2 = new Vector2(dorigin.x, dorigin.y);
-
-        return mapa.transform.TransformPoint(unityPosition2);
-
-
+        Vector2 unityPosition = new Vector2(dorigin.x, dorigin.y) / 50;
+        return person.transform.TransformPoint(unityPosition);
     }
 
-}
+} */
