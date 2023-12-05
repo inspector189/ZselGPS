5fa937ae5acd8ba9fc2eac8eedb3b5f23a431a73
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2023-12-05 12:09:46 +0100

    zmiana tła + poprawienie panelu

--- Assets/Script/SlidePanel.cs
+++ Assets/Script/SlidePanel.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 
 public class SlidePanel : MonoBehaviour
 {
-    public float speed = 1.0f; // Prêdkoœæ przesuwania
+    public float speed = 1.0f;
+    public float speed2 = 1.0f;// Prêdkoœæ przesuwania
     public GameObject panel; // Panel, który bêdzie przesuwany
+    public GameObject panel2; // Panel, który bêdzie przesuwany
 
     public bool isOpen = false; // Czy panel jest otwarty
-    private float closedPositionY = -1204.901f; // Pozycja zamkniêcia
-    private float openPositionY = -525.1f; // Pozycja otwarcia
+
+    public float closedPositionY = -1204.901f; // Pozycja zamkniêcia
+    public float openPositionY = -525.1f; // Pozycja otwarcia
     private float targetPositionY; // Docelowa pozycja Y panelu
 
-    private float closedHeight = 214.6f; // Wysokoœæ przy zamkniêtym panelu
-    private float openHeight = 3736.8f; // Wysokoœæ przy otwartym panelu
+    public float closedPositionY2 = 0f; // Pozycja zamkniêcia
+    public float openPositionY2 = 355f; // Pozycja otwarcia
+    private float targetPositionY2; // Docelowa pozycja Y panelu
+
+    public float closedHeight = 214.6f; // Wysokoœæ przy zamkniêtym panelu
+    public float openHeight = 3736.8f; // Wysokoœæ przy otwartym panelu
     private float targetHeight; // Docelowa wysokoœæ panelu
 
     private RectTransform rectTransform; // RectTransform panelu
+    private RectTransform rectTransform2; // RectTransform panelu
 
     void Start()
     {
@@ -26,6 +34,7 @@ public class SlidePanel : MonoBehaviour
         }
 
         rectTransform = panel.GetComponent<RectTransform>();
+        rectTransform2 = panel2.GetComponent<RectTransform>();
         if (rectTransform == null)
         {
             Debug.LogError("Panel does not have a RectTransform component");
@@ -34,20 +43,26 @@ public class SlidePanel : MonoBehaviour
 
         // Ustaw pocz¹tkow¹ pozycjê i wysokoœæ panelu
         rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, closedPositionY);
+
+        rectTransform2.anchoredPosition = new Vector2(rectTransform2.anchoredPosition.x, closedPositionY2);
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, closedHeight);
 
         targetPositionY = closedPositionY;
+        targetPositionY2 = closedPositionY2;
         targetHeight = closedHeight;
     }
 
     void Update()
     {
         
-        if (rectTransform != null)
+        if (rectTransform != null && rectTransform2 != null)
         {
             // Animuj pozycjê Y panelu
             float newY = Mathf.Lerp(rectTransform.anchoredPosition.y, targetPositionY, Time.deltaTime * speed);
             rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, newY);
+
+            float newY2 = Mathf.Lerp(rectTransform2.anchoredPosition.y, targetPositionY2, Time.deltaTime * speed2);
+            rectTransform2.anchoredPosition = new Vector2(rectTransform2.anchoredPosition.x, newY2);
 
             // Animuj wysokoœæ panelu
             float newHeight = Mathf.Lerp(rectTransform.sizeDelta.y, targetHeight, Time.deltaTime * speed);
@@ -64,6 +79,7 @@ public class SlidePanel : MonoBehaviour
 
                 isOpen = !isOpen;
             targetPositionY = isOpen ? openPositionY : closedPositionY;
+            targetPositionY2 = isOpen ? openPositionY2 : closedPositionY2;
             targetHeight = isOpen ? openHeight : closedHeight;
             PlayerPrefs.SetInt("panelStatus", 0);
         }
