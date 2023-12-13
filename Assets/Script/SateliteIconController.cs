7798f6077debfbe6893b6bedb0f2c2f086d4e130
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2023-12-13 01:15:54 +0100

    Ikonki w nawigacji na dole + sygnalizacja satelitki o słabym sygnale

---
+++ Assets/Script/SateliteIconController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SateliteIconController : MonoBehaviour
+{
+    public Image image;
+    
+    private Coroutine blinkingCoroutine = null;
+
+    private void Update()
+    {
+        float value = PlayerPrefs.GetFloat("accuracy");
+        if (value < 15)
+        {
+            SetColor(new Color(0, 1, 0, 1)); // Zielony, nieprzezroczysty
+        }
+        else if (value >= 15 && value < 45)
+        {
+            SetColor(new Color(1, 0, 0, 1)); // Czerwony, nieprzezroczysty
+        }
+        else if (value >= 45)
+        {
+            if (blinkingCoroutine == null)
+            {
+                blinkingCoroutine = StartCoroutine(BlinkingEffect());
+            }
+        }
+        else
+        {
+            if (blinkingCoroutine != null)
+            {
+                StopCoroutine(blinkingCoroutine);
+                blinkingCoroutine = null;
+            }
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+        if (blinkingCoroutine != null)
+        {
+            StopCoroutine(blinkingCoroutine);
+            blinkingCoroutine = null;
+        }
+        image.color = color;
+    }
+
+    private IEnumerator BlinkingEffect()
+    {
+        while (true)
+        {
+            float alpha = Mathf.PingPong(Time.time, 0.9f);
+            image.color = new Color(1, 0, 0, alpha); // Czerwony z p³ynnie zmieniaj¹cym siê alfa
+            yield return null;
+        }
+    }
+}
