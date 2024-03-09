3416e6c284b47d648e1e2cc9357f79d97901a590
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2024-03-09 20:49:21 +0100

    chrumchrumchrum

--- Assets/Script/QrCodeReader.cs
+++ Assets/Script/QrCodeReader.cs
@@ -7,9 +7,12 @@ using ZXing;
 using Vulcanova;
 using System.Security.Cryptography;
 using System.Text;
+using TMPro;
 
 public class QrCodeReader : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField tokentxt;
+    [SerializeField] private TMP_InputField pintxt;
     private const string QrKey = "tDVS4ykCBBAeN33h";
 
     public void PickImageAndScanQRCode()
@@ -34,8 +37,18 @@ public class QrCodeReader : MonoBehaviour
                     Debug.Log("Zdekodowany tekst z QR: " + result.Text);
                     try
                     {
+                        
                         string decodedText = DecodeQrCode(result.Text);
                         Debug.Log("Zdekodowany tekst z QR po AES: " + decodedText);
+                        ExtractValues(decodedText, out string token, out string miejscowosc);
+                        Debug.Log(token);
+                        Debug.Log(miejscowosc);
+                        if(miejscowosc == "wloclawek" && token.Length == 7)
+                        {
+                            tokentxt.text = token;
+                            tokentxt.Select();
+                            pintxt.Select();
+                        }
                     }
                     catch (Exception ex)
                     {
@@ -65,5 +78,16 @@ public class QrCodeReader : MonoBehaviour
 
         return reader.ReadToEnd();
     }
+    public void ExtractValues(string text, out string token, out string miejscowosc)
+    {
+        string[] parts = text.Split('#');
 
+        string urlPart = parts[1];
+
+        string[] urlParts = urlPart.Split('/');
+
+        miejscowosc = urlParts[urlParts.Length - 2];
+
+        token = parts[2];
+    }
 }
