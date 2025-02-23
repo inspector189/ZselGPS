a530a49b81c0571809e0c8cfb66eff8ff9e55957
Patryk <patrykowski8discord@gmail.com>
2025-02-23 22:08:28 +0100

    combining 3 scenes

--- Assets/Script/SceneManipulator/MainButtonOnMap.cs
+++ Assets/Script/SceneManipulator/MainButtonOnMap.cs
@@ -6,7 +6,6 @@ using TMPro;
 public class MainButtonOnMap : MonoBehaviour
 {
     public GameObject btn1;
-    public GameObject btn2;
     public GameObject btn3;
     // Start is called before the first frame update
     void Start()
@@ -15,19 +14,16 @@ public class MainButtonOnMap : MonoBehaviour
         if (wybor == 0)
         {
             btn1.SetActive(true);
-            btn2.SetActive(false);
             btn3.SetActive(false);
         }
         else if (wybor == 1)
         {
-            btn1.SetActive(false);
-            btn2.SetActive(true);
+            btn1.SetActive(true);
             btn3.SetActive(false);
         }
         else
         {
             btn1.SetActive(false);
-            btn2.SetActive(false);
             btn3.SetActive(true);
         }
     }
--- Assets/Script/ShowAndHidePanel.cs
+++ Assets/Script/ShowAndHidePanel.cs
@@ -9,20 +9,45 @@ public class ShowAndHidePanel : MonoBehaviour
     [SerializeField]
     private GameObject panel;
     public CanvasGroup panelCG;
+    [SerializeField]
+    private GameObject panel2;
+    public CanvasGroup panelCG2;
 
     public void ShowPanel()
     {
-        panel.SetActive(true);
-        panelCG.alpha = 1f;
-        panelCG.interactable = true;
-        panelCG.blocksRaycasts = true;
+        if (PlayerPrefs.GetInt("WybranaScena") == 0)
+        {
+            panel.SetActive(true);
+            panelCG.alpha = 1f;
+            panelCG.interactable = true;
+            panelCG.blocksRaycasts = true;
+        }
+        if (PlayerPrefs.GetInt("WybranaScena") == 1)
+        {
+            panel2.SetActive(true);
+            panelCG2.alpha = 1f;
+            panelCG2.interactable = true;
+            panelCG2.blocksRaycasts = true;
+        }
+
 
     }
     public void HidePanel()
     {
-        panel.SetActive(false);
-        panelCG.alpha = 0f;
-        panelCG.interactable = true;
-        panelCG.blocksRaycasts = false;
+        if (PlayerPrefs.GetInt("WybranaScena") == 0)
+        {
+            panel.SetActive(false);
+            panelCG.alpha = 0f;
+            panelCG.interactable = true;
+            panelCG.blocksRaycasts = false;
+        }
+        if (PlayerPrefs.GetInt("WybranaScena") == 1)
+        {
+            panel2.SetActive(false);
+            panelCG2.alpha = 0f;
+            panelCG2.interactable = true;
+            panelCG2.blocksRaycasts = false;
+        }
+
     }
 }
